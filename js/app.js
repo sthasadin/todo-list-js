02cdd96f7b57374de6306cf369e4de536711f4ef
sthasadin <shrestha.sadin007@gmail.com>
2020-08-18 17:57:27 +0545

    added transitio animation

--- js/app.js
+++ js/app.js
@@ -45,7 +45,11 @@ function deleteCheck(e){
   //Delete Todo
   if (item.classList[0] === "trash-btn"){
     const todo = item.parentElement;
-    todo.remove();
+    //Animation
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend',  function(){
+      todo.remove();
+    })
   }
   //check mark completed items
   if(item.classList[0] === "completed-btn"){
